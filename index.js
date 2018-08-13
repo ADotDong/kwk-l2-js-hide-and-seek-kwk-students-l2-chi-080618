eb6c18baaf96620b51c010c8b12d296cf52f58aa
ADotDong <amydong7777@gmail.com>
2018-08-13 16:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //Code your solution here
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('div')
+}
+
+function increaseRankBy(n){
+  parseInt(document.querySelector('ol')) + n
+}
